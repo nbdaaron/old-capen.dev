7e4003776997da6f2741f465dba783ede2ab0978
Aaron Kau <aaron.kau@rutgers.edu>
2019-04-22 12:31:16 +0000

    Add Handlebars and GraphQL Support

--- index.js
+++ index.js
@@ -3,9 +3,42 @@ const http = require('http');
 const https = require('https');
 const express = require('express');
 
+// GraphQL Dependencies
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+// GraphQL Types
+const Root = require('./types/root');
+
+// Handlebars
+const handlebars = require('express-handlebars');
+
+// Socket.IO
+const sio = require('socket.io');
+
+// Filesystem Dependencies
+const fs = require('fs');
+const path = require('path');
+
+// Imported Files
+const schemaFile = fs.readFileSync(path.resolve(__dirname, 'schema.graphql'), 'utf8');
 const config = require('./config');
 
+// TODO:
+// ABSTRACT ALL HTTP/HTTPS ROUTES INTO SEPARATE FILE
+// ABSTRACT ALL GRAPHQL SCHEMAS INTO SEPARATE FILE (SINGLE FILE INITIALLY. SEPARATE IF NEEDED)
+// ABSTRACT ALL SOCKET.IO CALLS/CONNECTIONS INTO SEPARATE FILES (PER-CONTENT BASIS)
+
 const app = express();
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use('/graphql', graphqlHTTP({
+	schema: buildSchema(schemaFile),
+	rootValue: new Root(),
+	graphiql: true
+}));
 
 const credentials = {
 	key: config.privateKey,
@@ -13,8 +46,12 @@ const credentials = {
 	ca: config.ca
 };
 
-app.use((req, res) => {
-	res.send('Hello world');
+// Routes
+
+app.get('/', (req, res) => {
+	res.render('home', {
+		a: "Capen"
+	});
 });
 
 // Starting both http & https servers
@@ -28,3 +65,15 @@ httpServer.listen(80, () => {
 httpsServer.listen(443, () => {
 	console.log('HTTPS Server running on port 443');
 });
+
+var io = sio.listen(httpsServer, credentials);
+
+// Socket Connections
+
+io.on('connection', (socket) => {
+	generateUser(socket);
+});
+
+function generateUser(socket) {
+	socket.emit('NewUsername', 'BOB');
+}
---
+++ types/root.js
@@ -0,0 +1,7 @@
+class Root {
+	hello() {
+		return "Hello World!";
+	}
+}
+
+module.exports = Root;
